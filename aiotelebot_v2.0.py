e35497f7f3105575e06ce6fcd52e95b3c26bc1b5
Ivan <vanikos@yandex.ru>
2022-02-10 13:02:49 +0300

    test gitignore

--- aiotelebot_v2.0.py
+++ aiotelebot_v2.0.py
@@ -46,9 +46,11 @@ async def start_using(message: types.Message):
         await bot.send_message(799592984, f'Кто-то нажал старт user_id - {message.from_user.id}, \n'
                                           f'user_name - {message.from_user.username}')
 
-@dp.message_handler(commands='help')
+@dp.message_handler()
 async def help_command(message: types.Message):
-    if
+    if message.text == 'Help':
+        if message.from_user.id == 799592984:
+            await message.answer('Работает пока что')
 
 
 
